7f205696c212b31fd43f48e1f3a9b32f0307da58
jano <jano_alex_es@hotmail.com>
2016-03-09 20:14:29 +0100

    Last refactor, get rid of magic numbers and add naming with some extra functions.

--- src/impresora/ImpresoraFactory.java
+++ src/impresora/ImpresoraFactory.java
@@ -1,23 +1,34 @@
 package impresora;
 
 public class ImpresoraFactory {
-    private int tipo_formato = 0;
+    private final int SIN_FORMATO = 0;
+    private final int FORMATO_HTML = 1;
+
+    private int tipo_formato = SIN_FORMATO;
     private int tipo_salida = 0;
 
     public ImpresoraFactory(String[] args) {
-        if(args.length > 0) {
-            this.tipo_formato = Integer.parseInt(args[0]);
-        }
-        if(args.length > 1) {
-            this.tipo_salida = Integer.parseInt(args[0]);
-        }
+        actualizarTipoFormato(args);
+        actualizarTipoSalida(args);
     }
 
     public Impresora build() {
-        if(tipo_formato == 0) {
+        if(tipo_formato == SIN_FORMATO) {
             return new ImpresoraSinFormato(tipo_salida);
-        } else {
+        }
+        if(tipo_formato == FORMATO_HTML) {
             return new ImpresoraHTML(tipo_salida);
         }
+        throw new IllegalArgumentException("Formato incorrecto");
+    }
+
+    private void actualizarTipoSalida(String[] args) {
+        if(args.length <= 1) return;
+        this.tipo_salida = Integer.parseInt(args[0]);
+    }
+
+    private void actualizarTipoFormato(String[] args) {
+        if(args.length == 0) return;
+        this.tipo_formato = Integer.parseInt(args[0]);
     }
 }
--- src/impresora/ImpresoraSinFormato.java
+++ src/impresora/ImpresoraSinFormato.java
@@ -4,18 +4,28 @@ import salida.Salida;
 import salida.SalidaConsola;
 import salida.SalidaImpresora;
 
-public class ImpresoraSinFormato implements Impresora {;
+public class ImpresoraSinFormato implements Impresora {
+    private final int SALIDA_CONSOLA = 0;
+    private final int SALIDA_IMPRESORA = 1;
+
     private Salida salida;
 
     public ImpresoraSinFormato(int tipo_salida) {
-        if (tipo_salida == 0) {
-            salida = new SalidaConsola();
-        } else {
-            salida = new SalidaImpresora();
-        }
+        salida = getSalida(tipo_salida);
     }
 
     public void imprimir(String mensaje) {
         this.salida.imprimir(mensaje);
+    }
+
+    private Salida getSalida(int tipo_salida) {
+        if (tipo_salida == SALIDA_CONSOLA) {
+            return new SalidaConsola();
+        }
+
+        if(tipo_salida == SALIDA_IMPRESORA){
+            return new SalidaImpresora();
+        }
+        throw new IllegalArgumentException("Tipo de salida de datos incorrecto");
     }
 }
